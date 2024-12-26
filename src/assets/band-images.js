d219ef6c78e1ef1ba8bedf64f532c988a93ce1e7
Mohammad Ajaruddin Shaik <shaikazaruddin9951@gmail.com>
2024-12-26 11:03:07 +0530

    solution

---
+++ src/assets/band-images.js
@@ -0,0 +1,7 @@
+const bandImages = [
+    { id: 1, type: "Solo Loop", image: "/assets/band-1.jpg" },
+    { id: 2, type: "Braided Solo Loop", image: "/assets/band-2.jpg" },
+    { id: 3, type: "Leather Loop", image: "/assets/band-3.jpg" },
+  ];
+  
+  export default bandImages;
---
+++ src/assets/case-images.js
@@ -0,0 +1,6 @@
+const caseImages = [
+    { id: 1, material: "Aluminum", image: "/assets/case-1.jpg" },
+    { id: 2, material: "Titanium", image: "/assets/case-2.jpg" },
+  ];
+  
+  export default caseImages;
